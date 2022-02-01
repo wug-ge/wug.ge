20f417aded2011e8a33b1451c7cb21176f00790e
Daniel Wuggenig <github@wug.ge>
2022-02-01 21:03:34 +0300

    fix: typo in about me

--- components/main-page-components/AboutMe.vue
+++ components/main-page-components/AboutMe.vue
@@ -11,7 +11,7 @@
             alt="Profile Picture" />
         </div>
         <div class="text-xl ml-6 md:w-4/12 md:mb-0 mb-4">
-I'm a fresh new freelancer &amp; digital nomad from Austria having just started in 2022. Still I have years (5+) of work experience in the industry as a Web Developer, Teamleader &amp; Project Manager in the industry.</div>
+I'm a fresh new freelancer &amp; digital nomad from Austria having just started in 2022. Still I have years (5+) of work experience in the industry as a Web Developer, Teamleader &amp; Project Manager.</div>
       </div>
     </div>
   </div>
