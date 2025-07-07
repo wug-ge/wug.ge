9917caf0d12520b7be1ae0c9f450de02e0bb9ef1
Daniel Wuggenig <git@wug.ge>
2025-07-07 20:17:11 +0300

    feat: update nuxt content to v3

--- frontend/pages/blog/[...slug].vue
+++ frontend/pages/blog/[...slug].vue
@@ -1,8 +1,16 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () =>
+  queryCollection('content').path(route.path).first()
+)
+</script>
+
 <template>
   <div>
     <index-page-header />
     <blog-content>
-      <ContentDoc />
+      <ContentRenderer v-if="page" :value="page" />
     </blog-content>
   </div>
 </template>
